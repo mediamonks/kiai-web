822fa602f8de2b8bed8b68e1ca41ad3235cc9906
Richard Heuser <richard.heuser@mediamonks.com>
2021-10-21 16:14:11 +0200

    wip TranscriberV2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import clamp from 'lodash/clamp';
+
 export type TMatchCandidates = {
 	[key: string]: string[];
 };
@@ -11,4 +13,16 @@ export function findMatch(input: string, candidates: TMatchCandidates, partial: 
 			return regex.test(input);
 		});
 	});
+}
+
+export function float32ToUint8(input: Float32Array) {
+	const output = new Uint8Array(input.length);
+
+	input.forEach((value, index) => {
+		let val = clamp(value, -1, 1);
+		val *= (val < 0 ? 0x8000 : 0x7FFF);
+		output[index] = (val / 256) + 128;
+	});
+
+	return output;
 }
