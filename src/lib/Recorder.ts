b2d47690b51511c8c69962c99e7432716b629653
Kevin van Helden <kevin@macbook-pro.home>
2021-11-18 12:45:00 +0100

    Recorder outputs correct data but only once after start init

--- src/lib/Recorder.ts
+++ src/lib/Recorder.ts
@@ -1,15 +1,18 @@
+/* eslint-disable max-len */
 import PipeSource from './PipeSource';
 
 type TRecorderOptions = {
 	bufferSize?: number;
 	sampleRate?: number;
 	channelCount?: number;
+	publishFrequency?: number;
 };
 
 const defaultOptions: TRecorderOptions = {
 	bufferSize: 1024,
 	sampleRate: 44100,
 	channelCount: 1,
+	publishFrequency: 200,
 };
 
 export default class Recorder extends PipeSource {
@@ -17,16 +20,28 @@ export default class Recorder extends PipeSource {
 	private isRecording: boolean = false;
 	private micStream: MediaStream;
 	private mediaRecorder: MediaRecorder;
+	private buffer: AudioBuffer;
 
 	constructor(options: TRecorderOptions = {}) {
 		super();
 
 		this.options = { ...defaultOptions, ...options };
+		this.buffer = new AudioBuffer({
+			length: this.options.bufferSize,
+			sampleRate: this.options.sampleRate,
+		});
 	}
 
 	public async start(): Promise<void> {
+		async function decodeAudio(arrayBuffer: ArrayBuffer): Promise<AudioBuffer> {
+			const ctx = new AudioContext();
+			const decodedAudio = await ctx.decodeAudioData(arrayBuffer, (audioBuffer) => audioBuffer);
+			return decodedAudio;
+		}
+
 		if (!this.micStream) {
 			this.micStream = await navigator.mediaDevices.getUserMedia({
+				video: false,
 				audio: {
 					sampleRate: this.options.sampleRate,
 					channelCount: this.options.channelCount,
@@ -34,22 +49,39 @@ export default class Recorder extends PipeSource {
 			});
 		}
 
-		if (!this.mediaRecorder) {
-			this.mediaRecorder = new MediaRecorder(this.micStream);
+		if (!this.mediaRecorder && this.micStream) {
+			this.mediaRecorder = new MediaRecorder(this.micStream, {
+				mimeType: 'audio/webm;codecs=opus',
+			});
 
-			this.mediaRecorder.ondataavailable = event => {
+			this.mediaRecorder.ondataavailable = async (event) => {
 				if (!this.isRecording) return;
-				this.publish(event.data);
+				const request = new XMLHttpRequest();
+				const url = URL.createObjectURL(event.data);
+				request.open('GET', url, true);
+				request.responseType = 'arraybuffer';
+				// eslint-disable-next-line require-atomic-updates
+				request.onload = async () => {
+					console.debug('response:', request.response);
+					this.buffer = await decodeAudio(request.response.slice(0));
+				};
+				request.send();
+				console.debug('buffer:', this.buffer);
+				this.publish(this.buffer.getChannelData(0));
 			};
 		}
 
-		this.mediaRecorder.start();
-		this.isRecording = true;
+		if (!this.isRecording) {
+			this.mediaRecorder.start(this.options.publishFrequency);
+			this.isRecording = true;
+		}
 	}
 
 	public stop(): void {
 		if (!this.mediaRecorder) return;
-		this.mediaRecorder.stop();
+		if (this.isRecording) {
+			this.mediaRecorder.stop();
+		}
 		this.isRecording = false;
 	}
 }
